61bedb54964be57258cafd449c846e9e031a5dc5
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-09-06 16:06:55 -0600

    created an IdeaStore method to handle database modifications

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require './idea'
+require './idea_store'
 
 class IdeaBoxApp < Sinatra::Base
 
@@ -10,7 +11,7 @@ class IdeaBoxApp < Sinatra::Base
 
 
   get '/' do
-    erb :index, locals: {ideas: Idea.all}
+    erb :index, locals: {ideas: IdeaStore.all, idea: Idea.new(params)}
   end
 
   not_found do
@@ -18,24 +19,22 @@ class IdeaBoxApp < Sinatra::Base
   end
 
   post '/' do
-    idea = Idea.new(params[:idea])
-    idea.save
+    IdeaStore.create(params[:idea])
     redirect '/'
   end
 
   delete '/:id' do |id|
-    Idea.delete(id.to_i)
+    IdeaStore.delete(id.to_i)
     redirect '/'
   end
 
   get '/:id/edit' do |id|
-    idea = Idea.find(id.to_i)
+    idea = IdeaStore.find(id.to_i)
     erb :edit, locals: {id: id, idea: idea}
   end
 
   put '/:id' do |id|
-    Idea.update(id.to_i, params['idea'])
+    IdeaStore.update(id.to_i, params['idea'])
     redirect '/'
   end
-
 end
--- idea.rb
+++ idea.rb
@@ -1,4 +1,3 @@
-require 'yaml/store'
 
 class Idea
   attr_reader :title, :description, :database
@@ -6,54 +5,5 @@ class Idea
   def initialize(attributes)
     @title = attributes["title"]
     @description = attributes["description"]
-  end
-
-  def save
-    database.transaction do |db|
-      db['ideas'] ||= []
-      db['ideas'] << {"title" => title, "description" => description}
-    end
-  end
-
-  def self.database
-    @database ||= YAML::Store.new "ideabox"
-  end
-
-  def self.all
-    raw_ideas.map do |data|
-      Idea.new(data)
-    end
-  end
-
-  def self.raw_ideas
-    database.transaction do |db|
-      db['ideas'] || []
-    end
-  end
-
-  def self.delete(position)
-    database.transaction do
-      database['ideas'].delete_at(position.to_i)
-    end
-  end
-
-  def self.find(id)
-    Idea.new(find_raw_idea(id))
-  end
-
-  def self.find_raw_idea(id)
-    database.transaction do
-      database['ideas'].at(id)
-    end
-  end
-
-  def self.update(id, data)
-    database.transaction do
-      database['ideas'][id] = data
-    end
-  end
-
-  def database
-    Idea.database
   end
 end
